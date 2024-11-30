3b2bad06fcabff96fb30121da82e08b365391290
Jahanvi Gupta <130801057+JahanviGupta17@users.noreply.github.com>
2024-11-30 15:09:28 +0530

    Update malaria_config.py

--- malaria_config.py
+++ malaria_config.py
@@ -26,28 +26,34 @@ Usage: import the module (see Jupyter notebooks for examples), or run from
     # Apply color splash to video using the last weights you trained
     python3 malaria.py splash --weights=last --video=<URL or path to file>
 """
-
 import os
 import sys
 import json
 import datetime
 import numpy as np
+import tensorflow as tf
 import skimage.draw
+import skimage.io
+import skimage.color
+import matplotlib.pyplot as plt
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("../../../")
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Import Mask RCNN
-sys.path.append(ROOT_DIR)  # To find local version of the library
+sys.path.append(ROOT_DIR)
 from mrcnn.config import Config
 from mrcnn import model as modellib, utils
 
-# Path to trained weights file
+# Path to COCO weights
 COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 
-# Directory to save logs and model checkpoints, if not provided
-# through the command line argument --logs
+# Directory for logs and checkpoints
 DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Check TensorFlow version
+if not tf.__version__.startswith('2'):
+    raise ImportError("TensorFlow 2.x is required. Found: {}".format(tf.__version__))
 
 ############################################################
 #  Configurations
@@ -55,127 +61,62 @@ DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 
 
 class MalariaConfig(Config):
-    """Configuration for training on the toy  dataset.
-    Derives from the base Config class and overrides some values.
-    """
-    # Give the configuration a recognizable name
+    """Configuration for training on the malaria dataset."""
     NAME = "malaria"
-
-    # We use a GPU with 12GB memory, which can fit two images.
-    # Adjust down if you use a smaller GPU.
     IMAGES_PER_GPU = 2
-
-    # Number of classes (including background)
-    NUM_CLASSES = 1 + 4  # Background + infected/uninfected
-
-    # Number of training steps per epoch
+    NUM_CLASSES = 1 + 4  # Background + classes
     STEPS_PER_EPOCH = 100
-
-    # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
-
-
 ############################################################
 #  Dataset
 ############################################################
-
 class CellDataset(utils.Dataset):
-
+    """Dataset for malaria cell detection."""
     def load_cell(self, dataset_dir, subset):
-        """Load a subset of the Malaria dataset.
-        dataset_dir: Root directory of the dataset.
-        subset: Subset to load: train or val
-        """
-        # Add classes. We have only one class to add.
+        """Load a subset of the dataset."""
         self.add_class("cell", 1, "uninfectedretic")
         self.add_class("cell", 2, "ring")
         self.add_class("cell", 3, "trophozoite")
         self.add_class("cell", 4, "schizont")
 
-        # Train or validation dataset?
         assert subset in ["train", "val"]
         dataset_dir = os.path.join(dataset_dir, subset)
 
-        # Load annotations
-        # VGG Image Annotator (up to version 1.6) saves each image in the form:
-        # { 'filename': '28503151_5b5b7ec140_b.jpg',
-        #   'regions': {
-        #       '0': {
-        #           'region_attributes': {},
-        #           'shape_attributes': {
-        #               'all_points_x': [...],
-        #               'all_points_y': [...],
-        #               'name': 'polygon'}},
-        #       ... more regions ...
-        #   },
-        #   'size': 100202
-        # }
-        # We mostly care about the x and y coordinates of each region
-        # Note: In VIA 2.0, regions was changed from a dict to a list.
         annotations = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))
-        annotations = list(annotations.values())  # don't need the dict keys
+        annotations = [a for a in annotations.values() if a['regions']]
 
-        # The VIA tool saves images in the JSON even if they don't have any
-        # annotations. Skip unannotated images.
-        annotations = [a for a in annotations if a['regions']]
-
-        # Add images
         for a in annotations:
-            # Get the x, y coordinaets of points of the polygons that make up
-            # the outline of each object instance. These are stores in the
-            # shape_attributes (see json format above)
-            # The if condition is needed to support VIA versions 1.x and 2.x.
-            if type(a['regions']) is dict:
-                polygons = [r['shape_attributes'] for r in a['regions'].values()]
-                objects = [s['region_attributes'] for s in a['regions'].values()]
-            else:
-                polygons = [r['shape_attributes'] for r in a['regions']]
-                objects = [s['region_attributes'] for s in a['regions']]
-
-
-            # load_mask() needs the image size to convert polygons to masks.
-            # Unfortunately, VIA doesn't include it in JSON, so we must read
-            # the image. This is only managable since the dataset is tiny.
+            polygons = [r['shape_attributes'] for r in a['regions'].values()]
+            objects = [s['region_attributes'] for s in a['regions'].values()]
             num_ids = [int(n['cell']) for n in objects]
+
             image_path = os.path.join(dataset_dir, a['filename'])
             image = skimage.io.imread(image_path)
             height, width = image.shape[:2]
 
             self.add_image(
                 "cell",
-                image_id=a['filename'],  # use file name as a unique image id
+                image_id=a['filename'],
                 path=image_path,
-                width=width, height=height,
+                width=width,
+                height=height,
                 polygons=polygons,
                 num_ids=num_ids)
 
     def load_mask(self, image_id):
-        """Generate instance masks for an image.
-       Returns:
-        masks: A bool array of shape [height, width, instance count] with
-            one mask per instance.
-        class_ids: a 1D array of class IDs of the instance masks.
-        """
-        # If not a malaria dataset image, delegate to parent class.
+        """Generate instance masks for an image."""
         image_info = self.image_info[image_id]
         if image_info["source"] != "cell":
             return super(self.__class__, self).load_mask(image_id)
         num_ids = image_info['num_ids']
-        # Convert polygons to a bitmap mask of shape
-        # [height, width, instance_count]
+
         info = self.image_info[image_id]
-        mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
-                        dtype=np.uint8)
+        mask = np.zeros([info["height"], info["width"], len(info["polygons"])], dtype=np.uint8)
         for i, p in enumerate(info["polygons"]):
-            # Get indexes of pixels inside the polygon and set them to 1
             rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
             mask[rr, cc, i] = 1
 
-        # Return mask, and array of class IDs of each instance. Since we have
-        # one class ID only, we return an array of 1s
-        num_ids = np.array(num_ids, dtype=np.int32)
-        #return mask.astype(np.bool), num_ids
-        return mask, num_ids
+        return mask, np.array(num_ids, dtype=np.int32)
 
     def image_reference(self, image_id):
         """Return the path of the image."""
@@ -183,31 +124,23 @@ class CellDataset(utils.Dataset):
         if info["source"] == "cell":
             return info["path"]
         else:
-            super(self.__class__, self).image_reference(image_id)
+            return super(self.__class__, self).image_reference(image_id)
 
-
-def train(model):
+def train(model, dataset_dir, subset):
     """Train the model."""
-    # Training dataset.
     dataset_train = CellDataset()
-    dataset_train.load_cell(args.dataset, "train")
+    dataset_train.load_cell(dataset_dir, "train")
     dataset_train.prepare()
 
-    # Validation dataset
     dataset_val = CellDataset()
-    dataset_val.load_cell(args.dataset, "val")
+    dataset_val.load_cell(dataset_dir, "val")
     dataset_val.prepare()
 
-    # *** This training schedule is an example. Update to your needs ***
-    # Since we're using a very small dataset, and starting from
-    # COCO trained weights, we don't need to train too long. Also,
-    # no need to train all layers, just the heads should do it.
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=50,
+                epochs=30,
                 layers='heads')
-
 
 def color_splash(image, mask):
     """Apply color splash effect.
